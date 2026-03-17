4e366f24120c6d9b5383e0bd0c31185a0718e536
Matt Jones <mattjones@bjornbears-mbp.home>
2026-03-17 16:31:00 +0000

    Implement dice rolling and player movement

--- src/saveThePlanet/Dice.java
+++ src/saveThePlanet/Dice.java
@@ -1,5 +1,19 @@
 package saveThePlanet;
+import java.util.Random;
 
 public class Dice {
+	
+    private Random random;
+
+    public Dice() {
+        random = new Random();
+    }
+
+    public int roll() {
+        return random.nextInt(6) + 1;
+    }
+	
+	
 
 }
+
